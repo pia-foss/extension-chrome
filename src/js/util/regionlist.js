98651647b3738be803f7da76265183ecfa0fe2e3
Ed <taesup63@gmail.com>
2018-09-04 21:03:08 -1000

    Better Offline startup support
    The extension is Private Browsing mode is currently working but is not
    in the best shape. Some recommended additions:
    
    * Don't auth in mockApp.js (This makes the extension auth each time
    it's opened in PB Mode)
    * Properly move the region data from Normal Mode into a PB Mode window
    * Only ask for region data from the API if it's not available at all
    * Update the mockApp.js init process to not rely on a polling mechanism

--- src/js/util/regionlist.js
+++ src/js/util/regionlist.js
@@ -1,10 +1,10 @@
-import tinyhttp from "tinyhttp";
+import tinyhttp from 'tinyhttp';
 
-export default function(app) {
+export default function (app) {
   const self = Object.create(null);
-  const http = tinyhttp("https://www.privateinternetaccess.com");
-  const {storage} = app.util;
-  const defaultRegionID  = "us_new_york_city";
+  const http = tinyhttp('https://www.privateinternetaccess.com');
+  const { storage } = app.util;
+  const defaultRegionID = 'us_new_york_city';
   const regionMap = new Map();
 
   const createLocalizedName = (regionID, region) => {
@@ -14,32 +14,36 @@ export default function(app) {
 
   const createRegion = (regionID, region) => {
     return {
-      scheme:        "https",
-      id:            regionID,
-      name:          region.name,
-      localizedName: () => createLocalizedName(regionID, region),
-      iso:           region.iso,
-      host:          region.dns,
-      port:          region.port,
-      macePort:      region.mace,
-      flag:          `/images/flags/${region.iso}_64.png`,
-      active:        false,
-      latency:       0,
-      offline:       false,
-      isFavorite:    false
+      scheme: 'https',
+      id: regionID,
+      name: region.name,
+      localizedName: () => { return createLocalizedName(regionID, region); },
+      iso: region.iso,
+      host: region.dns,
+      port: region.port,
+      macePort: region.mace,
+      flag: `/images/flags/${region.iso}_64.png`,
+      active: false,
+      latency: 0,
+      offline: false,
+      isFavorite: false,
     };
   };
 
-  self.synced  = false;
+  self.synced = false;
   self.syncing = false;
+
+  self.hasRegions = () => {
+    return !!regionMap.size;
+  };
 
   self.toArray = () => {
     return Array.from(regionMap.values());
-  }
+  };
 
   self.isSelectedRegion = (region) => {
     return self.getSelectedRegion().id === region.id;
-  }
+  };
 
   /*
     we keep an on-disk copy of the last selected region,
@@ -47,25 +51,29 @@ export default function(app) {
     not synced.
   */
   self.getSelectedRegion = () => {
-    const fromMemory  = self.toArray().find(region => region.active);
-    const fromStorage = storage.getItem("region");
-    if(fromMemory) { return fromMemory; }
-    if(fromStorage) {
+    const fromMemory = self.toArray().find((region) => { return region.active; });
+    const fromStorage = storage.getItem('region');
+    if (fromMemory) { return fromMemory; }
+    if (fromStorage) {
       const region = JSON.parse(fromStorage);
-      region.localizedName = () => createLocalizedName(region.id, region);
+      region.localizedName = () => { return createLocalizedName(region.id, region); };
       return region;
     }
-  }
+    return undefined;
+  };
 
   self.setSelectedRegion = (id) => {
     const region = regionMap.get(id);
-    if(region) {
-      self.toArray().forEach(_region => _region.active = false);
+    if (region) {
+      self.toArray().forEach((currentRegion) => {
+        const thisRegion = currentRegion;
+        thisRegion.active = false;
+      });
       region.active = true;
-      storage.setItem("activeproxy", region.id);
-      storage.setItem("region", JSON.stringify(region));
+      storage.setItem('activeproxy', region.id);
+      storage.setItem('region', JSON.stringify(region));
     }
-  }
+  };
 
   self.sync = () => {
     self.syncing = true;
@@ -73,29 +81,57 @@ export default function(app) {
     if (favoriteRegions) { favoriteRegions = favoriteRegions.split(','); }
     else { favoriteRegions = []; }
 
-    debug(`regionlist.js: start sync`);
-    return http.get("/api/client/services/https", {timeout: 5000}).then((xhr) => {
-      regionMap.clear();
-      const res = JSON.parse(xhr.response);
-      for(let regionID in res) {
-        let region = createRegion(regionID, res[regionID]);
-        if (favoriteRegions.includes(regionID)) { region.isFavorite = true; }
-        regionMap.set(regionID, region);
-      }
-      self.setSelectedRegion(storage.getItem("activeproxy") || defaultRegionID);
-      self.syncing = false;
-      self.synced = true;
-      app.util.bypasslist.updatePingGateways();
-      debug("regionlist.js: sync ok");
-      return xhr;
-    })
-    .catch((xhr) => {
-      self.syncing = false;
-      self.synced = false;
-      debug(`regionlist.js: sync error (${xhr.tinyhttp.cause})`);
-      return xhr;
+    debug('regionlist.js: start sync');
+    return http.get('/api/client/services/https', { timeout: 5000 })
+      .then((xhr) => {
+        regionMap.clear();
+        const res = JSON.parse(xhr.response);
+        Object.keys(res).forEach((regionID) => {
+          const region = createRegion(regionID, res[regionID]);
+          if (favoriteRegions.includes(regionID)) { region.isFavorite = true; }
+          regionMap.set(regionID, region);
+        });
+        self.setSelectedRegion(storage.getItem('activeproxy') || defaultRegionID);
+        self.syncing = false;
+        self.synced = true;
+        app.util.bypasslist.updatePingGateways();
+        debug('regionlist.js: sync ok');
+        return xhr;
+      })
+      .catch((xhr) => {
+        self.syncing = false;
+        self.synced = false;
+        debug(`regionlist.js: sync error (${xhr.tinyhttp.cause})`);
+        return xhr;
+      });
+  };
+
+  self.import = (regions) => {
+    if (!regions || !Array.isArray(regions)) { return; }
+    regionMap.clear();
+    regions.forEach((region) => {
+      const newRegion = createRegion(region.id, region);
+      regionMap.set(region.id, newRegion);
     });
-  }
+
+    self.syncing = false;
+    self.synced = true;
+    app.util.bypasslist.updatePingGateways();
+  };
+
+  self.export = () => {
+    return Array.from(regionMap.values())
+      .map((region) => {
+        return {
+          id: region.id,
+          name: region.name,
+          iso: region.iso,
+          dns: region.host,
+          port: region.port,
+          mace: region.macePort,
+        };
+      });
+  };
 
   self.setFavoriteRegion = (region) => {
     let currentFavs = storage.getItem('favoriteregions');
@@ -104,7 +140,7 @@ export default function(app) {
 
     if (!region.isFavorite) {
       currentFavs.push(region.id);
-      let favs = [...new Set(currentFavs)];
+      const favs = [...new Set(currentFavs)];
       storage.setItem('favoriteregions', favs.join(','));
     }
     else {
@@ -113,12 +149,12 @@ export default function(app) {
     }
 
     // update in memory region
-    let memRegion = self.toArray().find(r => r.id === region.id);
+    const memRegion = self.toArray().find((r) => { return r.id === region.id; });
     memRegion.isFavorite = !memRegion.isFavorite;
     return region;
-  }
+  };
 
-  chrome.alarms.create("PollRegionList", {delayInMinutes: 30, periodInMinutes: 30});
+  chrome.alarms.create('PollRegionList', { delayInMinutes: 30, periodInMinutes: 30 });
 
   return self;
 }
