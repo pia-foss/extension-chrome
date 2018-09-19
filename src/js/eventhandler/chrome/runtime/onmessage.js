560565049e0166212fbe13965ec387a7d818fae2
Brad Pfannmuller <bpfannmuller@londontrustmedia.com>
2018-09-19 23:14:30 +0000

    Resolve "Connfail page parity"

--- src/js/eventhandler/chrome/runtime/onmessage.js
+++ src/js/eventhandler/chrome/runtime/onmessage.js
@@ -1,31 +1,36 @@
-export default function(app) {
-  return function(msg, sender, sendResponse) {
-    switch(msg.request) {
-    case "RequestCredentials": {
-      const {user,i18n} = app.util,
-            domains = i18n.domainMap.values(),
-            allowedUrls = Array.from(domains).map((v) => `https://${v}/xpages/sign-in`)
-      if(user.authed && allowedUrls.includes(msg.url))
-        sendResponse({user: user.getUsername(), pass: user.getPassword()})
-      break
+export default function initOnMessage(app) {
+  return function onMessage(msg, sender, sendResponse) {
+    switch (msg.request) {
+      case 'RequestCredentials': {
+        const { user, i18n } = app.util;
+        const domains = i18n.domainMap.values();
+        const allowedUrls = Array.from(domains).map((v) => {
+          return `https://${v}/xpages/sign-in`;
+        });
+        if (user.authed && allowedUrls.includes(msg.url)) {
+          sendResponse({ user: user.getUsername(), pass: user.getPassword() });
+        }
+        break;
+      }
+      case 'RequestErrorInfo': {
+        const { errorinfo } = app.util;
+        sendResponse(errorinfo.get(msg.id));
+        break;
+      }
+      case 'RequestErrorDelete': {
+        const { errorinfo } = app.util;
+        errorinfo.delete(msg.id);
+        break;
+      }
+      case 't': {
+        const { i18n } = app.util;
+        const m = i18n.t(msg.localeKey);
+        sendResponse({ m });
+        break;
+      }
+      default: {
+        break;
+      }
     }
-    case "RequestErrorInfo": {
-      const errorinfo = app.util.errorinfo
-      sendResponse(errorinfo.get(msg.id))
-      break
-    }
-    case "RequestErrorDelete": {
-      const errorinfo = app.util.errorinfo
-      errorinfo.delete(msg.id)
-      break
-    }
-    case "t": {
-      const {i18n} = app.util,
-            m = i18n.t(msg.localeKey)
-      sendResponse({m})
-    }
-    default:
-      break
-    }
-  }
+  };
 }
